d0bcc779a5484ce8b72582e6b1cfdffe44213990
Suchit Sharma <gtsuchit7x@gmail.com>
2026-05-05 09:39:51 +0000

    spring: fix abnormal cpu temp

--- device.mk
+++ device.mk
@@ -123,7 +123,8 @@ TW_FRAMERATE            := 120
 # Blacklist Goodix fingerprint. There's no reason to include this input in recovery
 TW_INPUT_BLACKLIST := "uinput-goodix"
 
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone14/temp"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone17/temp"
+TW_BATTERY_SYSFS_WAIT_SECONDS := 6
 TW_BRIGHTNESS_PATH      := "/sys/class/backlight/panel0-backlight/brightness"
 
 TW_LOAD_VENDOR_MODULES  += "adsp_loader_dlkm.ko qpnp-smb5-main.ko 
