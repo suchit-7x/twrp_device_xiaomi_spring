58c12470720d199b0c1e41c1c4f6a4cc34c52e49
Suchit Sharma <gtsuchit7x@gmail.com>
2026-08-16 05:53:49 +0000

    spring: Drop hardcoded ccache path

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -75,16 +75,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ];
   
     # CCACHE
     export USE_CCACHE=1
-    export CCACHE_EXEC="/usr/bin/ccache"
-    export CCACHE_MAXSIZE="50G"
-    export CCACHE_DIR="/mnt/ccache"
-
-    # Warn if CCACHE_DIR is an invalid directory
-    if [ $USE_CCACHE = 1 ] && [ ! -d ${CCACHE_DIR} ];
-     then
-       echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
-       echo "Please edit the CCACHE_DIR build variable or mount the directory."
-    fi
 
     export LC_ALL="C"
     export BUILD_USERNAME=chickendrop89
